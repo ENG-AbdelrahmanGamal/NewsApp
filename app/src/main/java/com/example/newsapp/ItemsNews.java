43d8c2a9d94d085f8ca0b0b62a597a3d1944178b
Abdelrahman Gamal <abdelrahmanbetter@stud.cu.edu.eg>
2022-04-13 03:23:50 +0200

    news from scratch without any actions

---
+++ app/src/main/java/com/example/newsapp/ItemsNews.java
@@ -0,0 +1,13 @@
+package com.example.newsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ItemsNews extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
